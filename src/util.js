821cd5028de388965e124ffea9647f867e2973a3
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-07-06 19:59:14 -0500

    14. feat: generating a valid output

--- src/util.js
+++ src/util.js
@@ -12,5 +12,13 @@ const validateInput = (text, isEmpty, isNumber) => {
 
 const generateText = (name, age) => `${name} (${age} years old)`
 
+const checkAndGenerate = (name, age) => {
+    if(!validateInput(name, false, false) || !validateInput(age, false, false)){
+        return false;
+    }
+    return generateText(name, age)
+}
+
 exports.validateInput = validateInput;
 exports.generateText = generateText;
+exports.checkAndGenerate = checkAndGenerate;
