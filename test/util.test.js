529bc00db6e21bdcfce6a471e3e18ae73dfb46c1
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-07-06 19:46:32 -0500

    10. test: generating text

--- test/util.test.js
+++ test/util.test.js
@@ -1,8 +1,13 @@
-const { validateInput } = require('../src/util');;
+const { validateInput, generateText } = require('../src/util');;
 
 describe("Tests suite for utilites", () => {
     it("1. Validating form", () => {
         const validation = validateInput("sasa", false, false);
         expect(validation).toBeTruthy();
     })
+
+    it("2. Generating text", () => {
+        const generatedText = generateText("Miko", 27);
+        expect(generatedText).toBe('Miko (27 years old)')
+    });
 });
