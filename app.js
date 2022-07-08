70a89f3c1675a25e7bddbbcfc8d984a4a3e5db6f
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-07-08 14:13:45 -0500

    18. feat: writting app.js file

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-import './src/styles.css';
-const { hello, stylingButton } = require('./src/util')
+import "./src/styles.css";
+const {checkAndGenerate} = require("./src/util.js");
 
-stylingButton('input[type=submit]', {fontColor: 'white', bgColor: 'blue'});
-console.log(hello())
+const initApp = () => {
+  //const button = document.querySelector("#addPerson");
+  const form = document.querySelector("#formPerson");
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    addUser();
+  });
+};
+
+const addUser = () => {
+  const inpName = document.querySelector('#inpName').value;
+  const inpAge = document.querySelector('#inpAge').value;
+
+  const newText = checkAndGenerate(inpName, inpAge);
+
+  if(!newText) console.log('Nel');
+  else {
+    const newElement = createParragraph(newText);
+    document.querySelector('#result').appendChild(newElement);
+  }
+};
+
+const createParragraph = text => {
+  const parragraph = document.createElement('p');
+  parragraph.classList.add('item-name-list');
+  parragraph.textContent = text;
+  
+  return parragraph;
+}
+
+initApp();
