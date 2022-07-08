0d0313258d402017c0e48b6a2d50b88a69e9d2f4
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-07-08 14:07:45 -0500

    17: test: render an item on list

--- test/util.test.js
+++ test/util.test.js
@@ -1,4 +1,5 @@
-const { validateInput, generateText, checkAndGenerate } = require('../src/util');;
+const puppeteer = require('puppeteer');
+const { validateInput, generateText, checkAndGenerate } = require('../src/util');
 
 describe("Tests suite for utilites", () => {
     it("1. Validating form", () => {
@@ -12,8 +13,32 @@ describe("Tests suite for utilites", () => {
     });
 
     it("3. Generating a valid text output", () => {
-        const validOutput = checkAndGenerate("Felicia", 36);
+        const validOutput = checkAndGenerate("Felicia", "36");
         expect(validOutput).not.toBeFalsy();
         expect(validOutput).toBe('Felicia (36 years old)');
     })
+
+    it("4. Testing Puppeteer", async () => {
+        const browser = await puppeteer.launch({
+            args: ['--window-size=1280,1024'],
+            headless: false,
+            slowMo: 80
+        });
+        const page = await browser.newPage();
+        await page.goto('http://localhost:8080');
+
+        await page.click('input#inpName')
+        await page.type('input#inpName', 'Miko', {delay: 100});
+        await page.click('input#inpAge')
+        await page.type('input#inpAge', '32', {delay: 100});
+        await page.click('input[type=submit]');
+
+        const result = await page.$eval('.item-name-list', item => item.textContent);
+
+        expect(result).toBe("Miko (32 years old)");
+
+        //await page.waitForNavigation();
+        //await page.screenshot({path: 'example.png'})
+        //await browser.close();
+    }, 100000)
 });
